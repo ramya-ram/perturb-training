f36a01d50daa56f42e996fa0d60842cc838bdae9
Ramya <ram.ramya@gmail.com>
2015-04-11 17:50:12 -0400

    Adding Location class

---
+++ src/code/Location.java
@@ -0,0 +1,36 @@
+package code;
+
+public class Location{
+	public int row;
+	public int col;
+	
+	public Location(int row, int col){
+		this.row = row;
+		this.col = col;
+	}
+	
+	public int getRow() {
+		return row;
+	}
+	
+	public int getCol() {
+		return col;
+	}
+
+	public boolean equals(Object obj) {
+		Location otherLoc = (Location) obj;
+		return otherLoc.row == row && otherLoc.col == col;
+	}
+	
+	public String toString() {
+		return "("+row+", "+col+") ";
+	}
+	
+	public int hashCode() {
+		return 4;
+	}
+	
+	public Location clone(){
+		return new Location(row, col);
+	}
+}
