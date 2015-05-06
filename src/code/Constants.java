5490d3a36d898847468543ce3d7f9c3519d4b158
Ramya <ram.ramya@gmail.com>
2015-05-06 14:24:21 -0400

    Changing to navigation domain

--- src/code/Constants.java
+++ src/code/Constants.java
@@ -41,36 +41,6 @@ public class Constants {
 
 	/*public static Location[] trainingGoalLocs = {new Location(0,NUM_COLS-1), new Location(NUM_ROWS-1, 0), new Location(NUM_ROWS-1, NUM_COLS-1)};
 	public static Location[] testingGoalLocs = {new Location(2,NUM_COLS-2), new Location(NUM_ROWS-2, 2), new Location(NUM_ROWS-3, NUM_COLS-1)};
-	
-	public static List<Location> tokenLocs1 = Arrays.asList(new Location(3,3), new Location(2,4), new Location(8,8), new Location(6,7), new Location(7,2), new Location(1,8), new Location(2,8), new Location(3,8),
-			new Location(5,5), new Location(1,1), new Location(2,0));
-	public static List<Location> pitLocs1 = Arrays.asList(new Location(2,3), new Location(3,4), new Location(5,6), new Location(6,6), new Location(9,1), new Location(8,9));
-	
-	public static List<Location> tokenLocs2 = Arrays.asList(new Location(0,0), new Location(2,4), new Location(8,8), new Location(6,7), new Location(7,2), new Location(1,6), new Location(2,6), new Location(3,6),
-			new Location(9,8), new Location(5,5), new Location(7,0));
-	public static List<Location> pitLocs2 = Arrays.asList(new Location(2,3), new Location(3,4), new Location(0,2), new Location(6,6), new Location(7,1), new Location(7,9));
-	
-	public static List<Location> tokenLocs3 = Arrays.asList(new Location(0,0), new Location(2,4), new Location(5,2), new Location(5,3), new Location(5,4), new Location(1,6), new Location(2,6), new Location(3,6),
-			new Location(9,8), new Location(5,5), new Location(7,0));
-	public static List<Location> pitLocs3 = Arrays.asList(new Location(1,1), new Location(3,4), new Location(7,6), new Location(7,7), new Location(7,8), new Location(7,9));
-	
-	public static List<Location> tokenLocsTest1 = Arrays.asList(new Location(1,2), new Location(1,3), new Location(1,4), new Location(5,5), new Location(5,4), new Location(1,6), new Location(7,8), new Location(3,9),
-			new Location(9,8), new Location(5,5), new Location(7,0));
-	public static List<Location> pitLocsTest1 = Arrays.asList(new Location(4,1), new Location(7,4), new Location(7,6), new Location(7,7), new Location(7,8), new Location(7,9));
-	
-	public static List<Location> tokenLocsTest2 = Arrays.asList(new Location(1,2), new Location(1,3), new Location(1,5), new Location(5,5), new Location(5,4), new Location(1,6), new Location(7,8), new Location(3,9),
-			new Location(9,8), new Location(5,5), new Location(7,0));
-	public static List<Location> pitLocsTest2 = Arrays.asList(new Location(4,2), new Location(7,6), new Location(7,2), new Location(7,7), new Location(7,8), new Location(7,9));
-	
-	public static List<Location> tokenLocsTest3 = Arrays.asList(new Location(1,2), new Location(1,3), new Location(1,5), new Location(5,5), new Location(5,4), new Location(1,6), new Location(7,8), new Location(3,9),
-			new Location(5,0), new Location(3,0), new Location(4,0));
-	public static List<Location> pitLocsTest3 = Arrays.asList(new Location(4,2), new Location(7,6), new Location(7,3), new Location(6,7), new Location(6,8), new Location(6,9));
-	
-	public static List<List<Location>> allTokenLocs = Arrays.asList(tokenLocs1, tokenLocs2, tokenLocs3);
-	public static List<List<Location>> allPitLocs = Arrays.asList(pitLocs1, pitLocs2, pitLocs3);
-	
-	public static List<List<Location>> allTokenLocsTest = Arrays.asList(tokenLocsTest1, tokenLocsTest2, tokenLocsTest3);
-	public static List<List<Location>> allPitLocsTest = Arrays.asList(pitLocsTest1, pitLocsTest2, pitLocsTest3);
 	*/
 	public static final int NUM_TRAINING_SESSIONS = 3;
 	public static final int NUM_TESTING_SESSIONS = 3;
@@ -86,10 +56,10 @@ public class Constants {
 	public static String participantDir = "C:\\ExperimentData_Dec2014\\";
 	
 	public static String simulationDir = "C:\\Extra\\";
-	public static String rewardProceQName = simulationDir+"PQ_grid_changingTokensPits_500000train_3000test_30steps_ChooseBestInitialQValues_RandomLocsGoal.csv";
-	public static String rewardPerturbQName = simulationDir+"BQ_grid_changingTokensPits_500000train_3000test_30steps_ChooseBestInitialQValues_RandomLocsGoal.csv";
-	public static String rewardHRPerturbName = simulationDir+"BH_grid_changingTokensPits_500000train_3000test_30steps_ChooseBestInitialQValues_RandomLocsGoal.csv";
-	public static String rewardPRQLName = simulationDir+"PRQL_grid_changingTokensPits_500000train_3000test_30steps_ChooseBestInitialQValues_RandomLocsGoal.csv";
+	public static String rewardProceQName = simulationDir+"PQ.csv";
+	public static String rewardPerturbQName = simulationDir+"BQ.csv";
+	public static String rewardHRPerturbName = simulationDir+"BH_navigation.csv";
+	public static String rewardPRQLName = simulationDir+"PRQL_navigation.csv";
 	
 	public static String qvaluesDir = simulationDir;
 	
