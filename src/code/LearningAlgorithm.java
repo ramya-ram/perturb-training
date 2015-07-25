886b60a1e0c822aa8cba150f3358569fe922abb0
Ramya <ram.ramya@gmail.com>
2015-07-25 02:11:16 -0400

    Running fire task with all conditions

--- src/code/LearningAlgorithm.java
+++ src/code/LearningAlgorithm.java
@@ -696,7 +696,7 @@ public class LearningAlgorithm {
 						//System.out.println(currQValues.numNonZeros());
 						//System.out.println("episodeNum "+episodeNum);
 						int closestMDPNum = (int)((double[]) Main.proxy.returningFeval("runRBM", 1, Main.RBMTrainTaskData, Main.RBMTestTaskData[myWorld.sessionNum-1], 5)[0])[0] - 1;
-						System.out.println("task "+(myWorld.sessionNum-1)+"closestMDP "+closestMDPNum);
+						System.out.println("task "+(myWorld.sessionNum-1)+" closestMDP "+(closestMDPNum+1));
 						currQValues = trainedLearners.get(closestMDPNum).clone();
 						//System.out.println(trainedLearners.get(closestMDPNum).numNonZeros());
 						//System.out.println(currQValues.numNonZeros());
