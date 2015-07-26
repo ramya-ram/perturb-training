ed5d2f796e7cc426d4fc4e05cc657514ed52a4b4
Ramya <ram.ramya@gmail.com>
2015-07-26 01:43:56 -0400

    GridWorld reward limited time

--- src/code/Constants.java
+++ src/code/Constants.java
@@ -26,14 +26,14 @@ public class Constants {
 	public static final Scanner scan = new Scanner(System.in);
 	
 	public static final int NUM_EPISODES = 500000; //number of episodes agent simulates in the training task before working with the person (works with the person twice for each training task)
-	public static final int NUM_EPISODES_TEST = 500000; //number of episodes the agent simulates in the testing task before evaluating
+	public static final int NUM_EPISODES_TEST = 5000; //number of episodes the agent simulates in the testing task before evaluating
 	public static final int INTERVAL = 1000; //when showing reward over time (Main.INPUT == Main.REWARD_OVER_ITERS), reward is only recorded at every interval (e.g. every 100 iterations)
 	public static final int NUM_STEPS_PER_EPISODE = 30; //max number of steps the agent can have in each episode (after this, the agent stops the current execution and goes to the next episode)
 	
 	public static boolean usePredefinedTestCases = false;
 	public static boolean useOfflineValues = false;
 	public static int NUM_AVERAGING = 50; //run simulations this many times and average to get a more robust result
-	public static int NUM_RBM_DATA_POINTS = 100000; //number of data points that are sampled and given as input to the RBM (recording ALL might be too much data, so this can be some subset of the total number of <s,a,s'> the agent experiences)
+	public static int NUM_RBM_DATA_POINTS = 10000; //number of data points that are sampled and given as input to the RBM (recording ALL might be too much data, so this can be some subset of the total number of <s,a,s'> the agent experiences)
 	
 	public static int NUM_ROWS = 10;
 	public static int NUM_COLS = 10;
